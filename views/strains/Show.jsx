3df8dbcc5139a0e03a5376748dc5b00c8226436b
christine iyer <edithbird5@gmail.com>
2022-10-18 17:54:45 -0400

    really can't do a date

--- views/strains/Show.jsx
+++ views/strains/Show.jsx
@@ -1,6 +1,10 @@
+
 const React = require('react');
-const Default = require('../layouts/Default.jsx')
-const Moment = require('moment');
+const Default = require('../layouts/Default.jsx');
+const moment = require('moment')
+
+
+
 
 class Show extends React.Component {
    
@@ -8,14 +12,19 @@ class Show extends React.Component {
         const {Strain, Effects,Type, Rating, _id,Comments, Date} = this.props.strain
         const capName = Strain.toUpperCase() 
         const effectsName = Effects.toLowerCase()
+
+
+
+
+        
         
         // convertDate(Date){
         //     Date.moment().format('MMMM Do YYYY, h:mm:ss a');
         // }
     
         return(
-        <Default title={`${capName} Show Page`} strain={this.props.strain}>
-            <p>{capName} is {Type} and according to data, it can make you feel {effectsName}. New thoughts? {Comments} last edited </p>
+        <Default title={`${Strain} Show Page`} strain={this.props.strain}>
+            <p>{Strain} is {Type} and according to data, it can make you feel {effectsName}. New thoughts? {Comments} last edited </p>
         </Default>
         )
    } 
